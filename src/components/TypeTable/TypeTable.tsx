6cfa2530449c7ac85eb9c04a96a1cfab02f5bb25
OneSlightWeirdo <www.aereregion.webs.com@live.co.uk>
2020-10-16 17:56:50 +0100

    Made TypeTable form identifiers resemble species cards

--- src/components/TypeTable/TypeTable.tsx
+++ src/components/TypeTable/TypeTable.tsx
@@ -16,10 +16,12 @@ function Types(props:TypesProps) {
             <tbody>
                 {Object.entries(props.data).map((item)=>{
                     return <tr key={item[0]}>
-                        <th className="speciesDetailsTableCell">
-                            <img src={props.sprites[item[0]]} />
-                            {item[0]+":"}
-                        </th>
+                        <td className="speciesCard fill">
+                            <h4 className="speciesTitle grey">
+                                {item[0]+":"}
+                            </h4>
+                            <img className="spritePlaceholder right" src={props.sprites[item[0]]} />
+                        </td>
                         <td className="speciesDetailsTableCell">
                             {item[1].map((type)=>{
                                 return <TypeIcon key={type} type={type} />
